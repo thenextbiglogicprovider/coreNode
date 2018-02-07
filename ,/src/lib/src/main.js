d4753438687eb7880ebb7a4178e0a5f5db692d7d
Ajeet Yadav <maestro7007@outlook.com>
2018-02-08 01:17:34 +0530

    added dummy call to api

--- ,/src/lib/src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("../dist/scripts/server");
-const homeController_1 = require("./controllers/homeController");
-const appServer = server_1.AppServer;
-appServer.app.get('/', homeController_1.HomeController.Get());
--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,53 +3,68 @@ import {
     Response,
     Router,
 } from "express";
-
 import * as path from "path";
+import * as spider from "simplecrawler";
 
-import { Utils } from "../config/utils";
+import {
+    Utils,
+} from "../config/utils";
 import * as baseControllerModule from "./baseController";
 
 // tslint:disable-next-line:no-namespace
 export namespace Controllers {
-class HomeController extends baseControllerModule.Controllers.BaseController {
-    /**
-     *
-     */
-    constructor() {
-        super("Home");
-    }
-    public ProcessRequest(): Router {
-        this.ROUTER.route("/");
-        this.ROUTER.get("/", this.Get("/"));
-        this.ROUTER.get("/about", this.About("/about"));
-        return this.ROUTER;
-    }
+    const webSpider = spider;
+    class HomeController extends baseControllerModule.Controllers.BaseController {
+        /**
+         *
+         */
+        constructor() {
+            super("Home");
+        }
+        public ProcessRequest(): Router {
+            this.ROUTER.route("/");
+            this.ROUTER.get("/", this.Get("/"));
+            this.ROUTER.get("/about/:symbol", this.About("/about/:symbol"));
+            return this.ROUTER;
+        }
 
-    public GetViewPath(): string {
-        return this.VIEW_PATH;
+        public GetViewPath(): string {
+            return this.VIEW_PATH;
+        }
+        public Get(route: string): Router {
+            return this.ROUTER.get("/", (req: Request, res: Response) => {
+                this.CURRENT_VIEW_PATH = this.VIEW_PATH.replace("{1}", "index");
+                res.render(this.CURRENT_VIEW_PATH, {
+                    locals: {
+                        title: "Home",
+                        userData: req.session.userData,
+                    },
+                });
+            });
+        }
+        public About(route: string) {
+            return this.ROUTER.get(route, (req: Request, res: Response) => {
+
+                const request = require("request");
+                const symbol = req.params.symbol;
+                // tslint:disable-next-line:max-line-length
+                const url = "https://www.quandl.com/api/v3/datasets/NSE/" + `${symbol}` + ".json?api_key=YbWHxp_V2AvLbqPPczx3";
+                // tslint:disable-next-line:no-console
+                console.log(url);
+                request.get(url, {json: true}, (err, resp, body) => {
+                    res.send(body);
+                });
+            });
+        }
+        public Put(route: string): Router {
+            throw new Error("Method not implemented.");
+        }
+        public Post(route: string): Router {
+            throw new Error("Method not implemented.");
+        }
+        public Delete(route: string): Router {
+            throw new Error("Method not implemented.");
+        }
     }
-    public Get(route: string): Router {
-        return this.ROUTER.get("/", (req: Request, res: Response) => {
-            this.CURRENT_VIEW_PATH = this.VIEW_PATH.replace("{1}", "index");
-            res.render(this.CURRENT_VIEW_PATH, {locals: { title: "Home", userData: req.session.userData}});
-        });
-    }
-    public About(route: string) {
-        return this.ROUTER.get("/about", (req: Request, res: Response) => {
-            //this.CURRENT_VIEW = this.VIEW_PATH.replace("{0}", "home").replace("{1}", "index");
-            //res.render(this.CURRENT_VIEW, {locals: { title: "Home"}});
-            res.send("Hello from home");
-        });
-    }
-    public Put(route: string): Router {
-        throw new Error("Method not implemented.");
-    }
-    public Post(route: string): Router {
-        throw new Error("Method not implemented.");
-    }
-    public Delete(route: string): Router {
-        throw new Error("Method not implemented.");
-    }
-}
-export const homeController = new HomeController();
+    export const homeController = new HomeController();
 }
